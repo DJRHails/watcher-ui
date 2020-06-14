6fe77b48c9ab285b699d386b1aab57dbf5d4770f
DJRHails <daniel@hails.info>
2020-06-14 23:00:56 +0100

    Add Image component

---
+++ src/components/Image/Image.stories.tsx
@@ -0,0 +1,18 @@
+import React from "react"
+import { Image } from "../Image"
+import { StoryMeta } from "../../types/storybook"
+
+export default {
+  title: "Components/Image",
+  component: Image
+}
+
+export const responsive: StoryMeta = () => (
+  <Image
+    src="https://source.unsplash.com/random/1024x768"
+    sx={{
+      width: ["100%", "50%"],
+      borderRadius: "lg"
+    }}
+  />
+)
---
+++ src/components/Image/Image.tsx
@@ -0,0 +1,17 @@
+import React, { FC } from "react"
+import { Box, BoxProps } from "../Box"
+
+export interface ImageProps
+  extends BoxProps,
+    Omit<React.HTMLProps<HTMLImageElement>, keyof BoxProps> {}
+
+export const Image: FC<ImageProps> = (props: ImageProps) => (
+  <Box
+    as="img"
+    {...props}
+    css={{
+      maxWidth: "100%",
+      height: "auto"
+    }}
+  />
+)

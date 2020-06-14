2430c0ad1bb95e0d3849ef78757711951cfe087f
DJRHails <daniel@hails.info>
2020-06-14 22:58:07 +0100

    Add additional type support

---
+++ src/types/rebass.d.ts
@@ -0,0 +1,52 @@
+// Based on: https://github.com/DefinitelyTyped/DefinitelyTyped/blob/0bd28530564c3da2e728518084f22648af3a683c/types/rebass/index.d.ts
+
+import { ResponsiveStyleValue, SystemStyleObject } from "@styled-system/css"
+import * as React from "react"
+import * as StyledComponents from "styled-components"
+import * as StyledSystem from "styled-system"
+
+export {}
+
+type Omit<T, K> = Pick<T, Exclude<keyof T, K>>;
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export interface BaseProps extends React.RefAttributes<any> {
+  as?: React.ElementType;
+  css?:
+    | StyledComponents.CSSObject
+    | StyledComponents.FlattenSimpleInterpolation
+    | string;
+}
+
+export type SxStyleProp =
+  | SystemStyleObject
+  | Record<
+      string,
+      | SystemStyleObject
+      | ResponsiveStyleValue<number | string>
+      | Record<
+          string,
+          SystemStyleObject | ResponsiveStyleValue<number | string>
+        >
+    >;
+
+export interface SxProps {
+  /**
+   * The sx prop lets you style elements inline, using values from your theme.
+   */
+  sx?: SxStyleProp;
+}
+
+export interface BoxKnownProps
+  extends BaseProps,
+    StyledSystem.SpaceProps,
+    StyledSystem.LayoutProps,
+    StyledSystem.FontSizeProps,
+    StyledSystem.ColorProps,
+    StyledSystem.FlexProps,
+    StyledSystem.OrderProps,
+    StyledSystem.AlignSelfProps,
+    SxProps {
+  variant?: StyledSystem.ResponsiveValue<string>;
+  tx?: string;
+}
---
+++ src/types/storybook.d.ts
@@ -0,0 +1,24 @@
+import { ReactNode } from "react"
+
+export type StoryId = string;
+export type StoryKind = string;
+export type StoryName = string;
+
+export type StoryFnType<Context, ReturnType> = (context?: Context) => ReturnType;
+
+
+export interface KindMeta<
+  Component = unknown
+> {
+  id?: StoryId;
+  title: StoryKind;
+  component?: Component;
+  subcomponents?: Record<string, Component>;
+}
+
+export interface StoryMeta {
+  (): ReactNode;
+  story?: {
+    name: string
+  }
+}

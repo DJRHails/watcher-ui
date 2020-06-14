d69fe296b8f042ce893d5c52ca318ece13a83282
DJRHails <daniel@hails.info>
2020-06-14 22:57:47 +0100

    First lint passthrough

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import React from "react";
-import "./App.css";
-import logo from "./logo.svg";
+import React, { ReactNode } from "react"
+import "./App.css"
+import logo from "./logo.svg"
 
-function App() {
+function App(): ReactNode {
   return (
     <div className="App">
       <header className="App-header">
@@ -20,7 +20,7 @@ function App() {
         </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App

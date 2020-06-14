6d68dae6f8c68bb99c39d83754a82c795e24fd2e
DJRHails <daniel@hails.info>
2020-06-14 22:59:31 +0100

    Add Button component

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -1,21 +1,22 @@
 // tslint:disable-next-line:no-implicit-dependencies
-import { action } from "@storybook/addon-actions";
-import React from "react";
-import { Button } from "./Button";
+import { action } from "@storybook/addon-actions"
+import React from "react"
+import { Button } from "../Button"
+import { StoryMeta } from "../../types/storybook"
 
 export default {
   title: "Components/Button",
   component: Button
-};
+}
 
-export const Text = () => (
+export const Text: StoryMeta = () => (
   <Button onClick={action("clicked")}>Hello Button</Button>
-);
+)
 
-export const Emoji = () => (
+export const Emoji: StoryMeta = () => (
   <Button onClick={action("clicked")}>
     <span role="img" aria-label="so cool">
       😀 😎 👍 💯
     </span>
   </Button>
-);
+)
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,17 +1,17 @@
-import React, { FC, ReactNode } from "react";
+import React, { FC } from "react"
+import { Box, BoxProps } from "../Box"
 
-interface ButtonProps {
-  children: ReactNode;
+interface ButtonProps extends BoxProps,
+Omit<React.HTMLProps<HTMLImageElement>, keyof BoxProps> {
   onClick?: VoidFunction;
 }
 
-// type ButtonProps = {
-//   children: ReactNode;
-//   onClick?: () => void;
-// }
-
-export const Button: FC<ButtonProps> = ({ children, onClick }: ButtonProps) => (
-  <button onClick={onClick} type="button">
+export const Button: FC<ButtonProps> = ({
+  children,
+  variant = "simple",
+  ...props
+}: ButtonProps) => (
+  <Box tx="buttons" variant={variant} as="button" {...props}>
     {children}
-  </button>
-);
+  </Box>
+)

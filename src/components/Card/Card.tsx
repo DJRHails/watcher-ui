bb960b56f33a093c266dff1793aaa52a5c14b9f2
DJRHails <daniel@hails.info>
2020-06-14 23:00:09 +0100

    Add Card component

---
+++ src/components/Card/Card.tsx
@@ -0,0 +1,27 @@
+import React, { FC } from "react"
+import { Box, BoxProps } from "../Box"
+import { CardVariants } from "../../themes/theme"
+import styled from "styled-components"
+import { boxShadow, BoxShadowProps } from "styled-system"
+
+export interface CardProps
+  extends BoxProps, BoxShadowProps,
+    Omit<React.HTMLProps<HTMLDivElement>, keyof BoxProps> {
+    variant?: CardVariants
+  }
+
+const CardBox = styled(Box)(boxShadow)
+
+export const Card: FC<CardProps> = ({
+  children,
+  variant = "default",
+  ...props
+}: CardProps) => (
+  <CardBox
+    tx="cards"
+    variant={variant}
+    {...props}
+  >
+    {children}
+  </CardBox>
+)

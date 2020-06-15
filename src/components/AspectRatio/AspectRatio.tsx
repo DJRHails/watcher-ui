fad1f5351f98c3226d307ee73bc8fac135329fa8
DJRHails <daniel@hails.info>
2020-06-15 22:29:25 +0100

    Add Aspect Ratio component

---
+++ src/components/AspectRatio/AspectRatio.tsx
@@ -0,0 +1,49 @@
+// Ref: https://chakra-ui.com/aspectratiobox
+
+import React from "react";
+import { BoxProps, Box } from "../Box";
+
+interface AspectRatioOptions {
+  ratio?: number
+}
+
+export interface AspectRatioProps extends BoxProps, AspectRatioOptions {}
+
+/**
+ * React component used to cropping media (videos, images and maps)
+ * to a desired aspect ratio.
+ */
+export const AspectRatio: React.FC<AspectRatioProps> = ({ ratio = 4 / 3, maxWidth="sm", children, ...rest }: AspectRatioProps) => {
+  const child = React.Children.only(children);
+
+  return (
+    <Box
+      maxWidth={maxWidth}
+      css={{
+        "&": {
+          position: "relative",
+          height: 0,
+          display: "block",
+          paddingBottom: `${(1 / ratio) * 100}%`,
+        },
+        "& > *": {
+          overflow: "hidden",
+          position: "absolute !important",
+          top: "0",
+          left: "0",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          width: "100%",
+          height: "100%",
+        },
+        "& > img, & > video": {
+          objectFit: "cover",
+        },
+      }}
+      {...rest}
+    >
+      {child}
+    </Box>
+  );
+};

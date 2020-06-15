9bc5e559d7d7f00fb2c19193d5a7f1b276a1c67d
DJRHails <daniel@hails.info>
2020-06-15 17:25:01 +0100

    Add remaining headings and document

--- src/components/Typography/Headings.stories.tsx
+++ src/components/Typography/Headings.stories.tsx
@@ -8,3 +8,14 @@ export default {
 };
 
 export const h1: StoryMeta = () => <Heading.h1>Heading h1</Heading.h1>;
+
+export const headingsList: StoryMeta = () => (
+  <>
+    <Heading.h1>Heading h1</Heading.h1>
+    <Heading.h2>Heading h2</Heading.h2>
+    <Heading.h3>Heading h3</Heading.h3>
+    <Heading.h4>Heading h4</Heading.h4>
+    <Heading.h5>Heading h5</Heading.h5>
+    <Heading.h6>Heading h6</Heading.h6>
+  </>
+);
--- src/components/Typography/Headings.tsx
+++ src/components/Typography/Headings.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Text, TextProps } from "./Text";
-import { TypographyVariants } from "../../themes/theme";
+import { TypographyVariants } from "../../themes/typography";
 
 export interface HeadingProps
   extends TextProps,
@@ -8,15 +8,62 @@ export interface HeadingProps
   variant?: TypographyVariants;
 }
 
-const h1: React.FC<HeadingProps> = props => <Text variant="h1" {...props} />;
+const Heading: React.FC<HeadingProps> = (props) => <Text variant="heading" {...props} />;
 
-// const h2: React.FC<TextProps> = (props) => (
-//   <Text variant="h2" {...props} />
-// );
+const h1: React.FC<HeadingProps> = (props) => (
+  <Heading
+    fontSize={7}
+    as="h1"
+    {...props}
+  />
+);
+const h2: React.FC<HeadingProps> = (props) => (
+  <Heading
+    fontSize={6}
+    as="h2"
+    {...props}
+  />
+);
+const h3: React.FC<HeadingProps> = (props) => (
+  <Heading
+    fontSize={5}
+    mt={2}
+    as="h3"
+    {...props}
+  />
+);
+const h4: React.FC<HeadingProps> = (props) => (
+  <Heading
+    fontSize={4}
+    mt={2}
+    as="h4"
+    {...props}
+  />
+);
+const h5: React.FC<HeadingProps> = (props) => (
+  <Heading
+    fontSize={3}
+    mt={1}
+    as="h5"
+    {...props}
+  />
+);
+const h6: React.FC<HeadingProps> = (props) => (
+  <Heading
+    fontSize={2}
+    mt={1}
+    as="h6"
+    {...props}
+  />
+);
 
-const Heading = {
-  h1
-  // h2: h2,
+const Headings = {
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6,
 };
 
-export default Heading;
+export default Headings;

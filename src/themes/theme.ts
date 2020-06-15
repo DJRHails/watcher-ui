f8cb52af6ea6a91902f81da71b261c5ea8086ebd
DJRHails <daniel@hails.info>
2020-06-15 17:23:40 +0100

    Add tailwind spacing

--- src/themes/theme.ts
+++ src/themes/theme.ts
@@ -45,6 +45,79 @@ export const radii = {
 
 export const space = [0, "0.25rem", "0.5rem", "1rem", "2rem", "4rem", "8rem"];
 
+const tailwindSpacing = {
+  px: "1px",
+  "0": "0",
+  "1": "0.25rem",
+  "2": "0.5rem",
+  "3": "0.75rem",
+  "4": "1rem",
+  "5": "1.25rem",
+  "6": "1.5rem",
+  "8": "2rem",
+  "10": "2.5rem",
+  "12": "3rem",
+  "16": "4rem",
+  "20": "5rem",
+  "24": "6rem",
+  "32": "8rem",
+  "40": "10rem",
+  "48": "12rem",
+  "56": "14rem",
+  "64": "16rem",
+};
+
+const tailwindMaxWidth = {
+  xs: "20rem",
+  sm: "24rem",
+  md: "28rem",
+  lg: "32rem",
+  xl: "36rem",
+  "2xl": "42rem",
+  "3xl": "48rem",
+  "4xl": "56rem",
+  "5xl": "64rem",
+  "6xl": "72rem",
+};
+
+const tailwindWidth = {
+  "1/2": "50%",
+  "1/3": "33.333333%",
+  "2/3": "66.666667%",
+  "1/4": "25%",
+  "2/4": "50%",
+  "3/4": "75%",
+  "1/5": "20%",
+  "2/5": "40%",
+  "3/5": "60%",
+  "4/5": "80%",
+  "1/6": "16.666667%",
+  "2/6": "33.333333%",
+  "3/6": "50%",
+  "4/6": "66.666667%",
+  "5/6": "83.333333%",
+  "1/12": "8.333333%",
+  "2/12": "16.666667%",
+  "3/12": "25%",
+  "4/12": "33.333333%",
+  "5/12": "41.666667%",
+  "6/12": "50%",
+  "7/12": "58.333333%",
+  "8/12": "66.666667%",
+  "9/12": "75%",
+  "10/12": "83.333333%",
+  "11/12": "91.666667%",
+};
+
+export const sizes = {
+  ...tailwindSpacing,
+  ...tailwindMaxWidth,
+  ...tailwindWidth,
+  full: "100%",
+  screenHeight: "100vh",
+  screenWidth: "100vw",
+};
+
 // TODO: Check support doesn't
 export const transitions = {
   shadow: "box-shadow 500ms",
@@ -129,15 +202,14 @@ export const letterSpacings = {
   widest: "0.1em"
 };
 
-export type TypographyVariants = "h1" | "body" | "button";
+export type TypographyVariants = "heading" | "body" | "button";
 export const typography = {
-  h1: {
+  heading: {
     fontFamily: "body",
     fontWeight: "bold",
-    lineHeight: "normal",
+    lineHeight: "none",
     m: 0,
     mb: 1,
-    fontSize: 6,
     mt: 4
   },
   body: {
@@ -161,19 +233,20 @@ const baseCard = {
 };
 
 export type CardVariants = "outline" | "default";
-export const cards = {
+const cards = {
   outline: {
     ...baseCard,
     "--outline-color": "rgb(226, 232, 240)",
-    boxShadow: "outline"
+    boxShadow: "outline",
   },
   default: {
     ...baseCard,
+    boxShadow: "md",
   }
 };
 
 export type Variants = "badge" | "link";
-export const variants = {
+const variants = {
   badge: {
     fontFamily: "body",
     textTransform: "uppercase",
@@ -202,6 +275,7 @@ const theme = {
   colors,
   radii,
   space,
+  sizes,
   transitions,
 
   // Variants

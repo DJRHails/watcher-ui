874861ba78e41420029630c79674d2b7f9c181ec
DJRHails <daniel@hails.info>
2020-06-17 15:19:11 +0100

    Add asset table

--- src/components/Table/Table.stories.tsx
+++ src/components/Table/Table.stories.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { StoryMeta } from "../../types/storybook";
 import { Table } from "./Table";
+import { Heading } from "../Typography";
+import { Box } from "../Box";
+import { Asset } from "../Asset";
 
 export default {
   title: "Components/Table",
@@ -39,3 +42,65 @@ export const table: StoryMeta = () => (
     </tbody>
   </Table>
 );
+
+export const assetTable: StoryMeta = () => {
+  const assetRows = [
+    {
+      asset: "Eth",
+      balance: 0.3434,
+      price: 184.82
+    },
+    {
+      asset: "Chai",
+      balance: 0.9926,
+      price: 0.81
+    },
+    {
+      asset: "rDAI",
+      balance: 4.0604,
+      price: 0.80
+    },
+    {
+      asset: "sDEFI",
+      balance: 0.0013,
+      price: 1918.67
+    },
+    {
+      asset: "sUSD",
+      balance: 0.0031,
+      price: 1
+    },
+  ];
+  return (
+    <Box p={3}>
+      <Heading.h4>Wallet £69.95</Heading.h4>
+      <Table
+        m={0}
+        mt={3}
+        border={0}
+        height={1}
+      >
+        <thead>
+          <tr>
+            <th>Asset</th>
+            <th>Balance</th>
+            <th>Price</th>
+            <th>Value</th>
+          </tr>
+        </thead>
+        <tbody>
+          { assetRows.map((assetRow, idx) => (
+            <tr key={idx}>
+              <td>
+                <Asset name={assetRow.asset} />
+              </td>
+              <td>{assetRow.balance.toFixed(4)}</td>
+              <td>£{assetRow.price.toFixed(2)}</td>
+              <td>£{(assetRow.price * assetRow.balance).toFixed(2)}</td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    </Box>
+  );
+};

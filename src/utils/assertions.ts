09cbff09472eeac1718b6622902cd2b482102913
DJRHails <daniel@hails.info>
2020-06-19 14:31:44 +0100

    Add utilities for responsive and type assertions

---
+++ src/utils/assertions.ts
@@ -0,0 +1,41 @@
+/* eslint-disable @typescript-eslint/no-explicit-any,@typescript-eslint/explicit-module-boundary-types */
+
+export const isNull = (value: any): value is null => value == null;
+
+export function isString(value: any): value is string {
+  return Object.prototype.toString.call(value) === "[object String]";
+}
+
+export function isArray<T>(value: any): value is Array<T> {
+  return Array.isArray(value);
+}
+
+export const isEmptyArray = (value: any) => isArray(value) && value.length === 0;
+
+
+export const isEmpty = (value: any) => {
+  if (isArray(value)) return isEmptyArray(value);
+  if (isObject(value)) return isEmptyObject(value);
+  if (value == null || value === "") return true;
+  return false;
+};
+
+export const isObject = (value: any): value is Dict<any> => {
+  const type = typeof value;
+  return (
+    value != null &&
+    (type === "object" || type === "function") &&
+    !isArray(value)
+  );
+};
+
+export const isEmptyObject = (value: any) =>
+  isObject(value) && Object.keys(value).length === 0;
+
+export function isNotEmptyObject(value: any): value is Record<string, unknown> {
+  return value && !isEmptyObject(value);
+}
+
+export function isNumber(value: any): value is number {
+  return typeof value === "number";
+}
---
+++ src/utils/responsive.ts
@@ -0,0 +1,25 @@
+/* eslint-disable @typescript-eslint/no-explicit-any, @typescript-eslint/explicit-module-boundary-types */
+
+import { isArray, isObject } from "./assertions";
+
+export const objectKeys = <T extends Dict<any>>(obj: T) =>
+  (Object.keys(obj) as unknown) as (keyof T)[];
+
+export function mapResponsive(prop: any, mapper: (val: any) => any) {
+  if (isArray(prop)) {
+    return prop.map(mapper);
+  }
+
+  if (isObject(prop)) {
+    return objectKeys(prop).reduce((result: Dict, key) => {
+      result[key] = mapper(prop[key]);
+      return result;
+    }, {});
+  }
+
+  if (prop != null) {
+    return mapper(prop);
+  }
+
+  return null;
+}

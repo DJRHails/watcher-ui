694a35ad37c1bc128d6f21a22783a452f99b3b81
DJRHails <daniel@hails.info>
2020-06-23 10:31:17 +0100

    Update Icon interface to be more intuitive
    and handle size / fontsize distinction better.

--- src/components/Icon/Icon.stories.tsx
+++ src/components/Icon/Icon.stories.tsx
@@ -2,18 +2,54 @@ import React from "react";
 import { KindMeta, StoryMeta } from "../../types/storybook";
 
 import { Icon } from "../Icon";
+import { Flex, Text } from "..";
+import { ETHERSCAN_NAMESPACE } from "./Icon";
 
 const story: KindMeta<typeof Icon> = {
   title: "Components/Icon",
   component: Icon,
 };
 
-export const rimble: StoryMeta = () => (
-  <Icon name="Eth"/>
+export const brandEth: StoryMeta = () => (
+  <Icon name="Eth" color="brand"/>
+);
+
+export const check: StoryMeta = () => (
+  <Icon name="Check" color="success" /> 
+);
+
+export const automatic: StoryMeta = () => (
+  <>
+    <Flex mb={3}>
+      <Icon name="reallylongtoken"/>
+      <Text ml={2}>reallylongtoken</Text>
+    </Flex>
+    <Flex mb={3}>
+      <Icon name="spoink"/>
+      <Text ml={2}>spoink</Text>
+    </Flex>
+    <Flex mb={3}>
+      <Icon name="fake"/>
+      <Text ml={2}>fake</Text>
+    </Flex>
+    <Flex mb={3}>
+      <Icon name="THR"/>
+      <Text ml={2}>THR</Text>
+    </Flex>
+    <Flex mb={3}>
+      <Icon name="AX"/>
+      <Text ml={2}>AX</Text>
+    </Flex>
+    <Flex>
+      <Icon name="A"/>
+      <Text ml={2}>A</Text>
+    </Flex>
+    
+  </>
 );
 
 export const etherscan: StoryMeta = () => (
-  <Icon name="Chai"/>
+  <Icon name={`${ETHERSCAN_NAMESPACE}chai`}/>
 );
 
 export const algorand: StoryMeta = () => (
--- src/components/Icon/Icon.tsx
+++ src/components/Icon/Icon.tsx
@@ -1,88 +1,105 @@
 import React from "react";
-import { Icon as RimbleIcon, IconProps as RimbleIconProps, CryptoIcons } from "@rimble/icons";
-import { Image } from "..";
-import { Circle } from "..";
-import { Text } from "..";
+import { isWatcherIcon, WatcherIcon, WatcherIconProps } from "watcher-icons";
+import { Image, Circle, Text } from "..";
 import { Box, BoxProps } from "../Box";
+import styled from "styled-components";
+import { color, ColorProps } from "styled-system";
+import _ from "lodash";
+import { PropsOf } from "../../types/utils";
 
-export interface IconInnerProps extends Omit<RimbleIconProps, "name"> {
+export interface IconInnerProps extends Omit<WatcherIconProps, "name"> {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  textColor?: any; // TODO: Replace with Styled System color type
-  name: RimbleIconProps["name"] | "Algo"
+  color?: ColorProps["color"]; 
+  backgroundColor?: ColorProps["backgroundColor"];
+  size?: string;
+  name: string;
 }
 
 export interface IconProps extends BoxProps {
   name: string;
-  innerSize?: string;
-} 
+}
+
+const StyledWatcherIcon = styled(WatcherIcon)(
+  color,
+);
+
+export const ETHERSCAN_NAMESPACE = "ethscn.";
+const isEtherscanIcon = (name: string) => name.startsWith(ETHERSCAN_NAMESPACE);
+const etherscanTransform = (name: string) => name.substring(ETHERSCAN_NAMESPACE.length).toLowerCase();
+
+const watcherTransform = (name: string) => _.upperFirst(_.camelCase(name));
+
+const LEN2FONT = {
+  0: "1.em",
+  1: ".6em",
+  2: ".5em",
+  3: ".4em",
+  4: ".3em",
+};
 
 const IconInner: React.FC<IconInnerProps> = ({
   name,
-  size = "2em",
+  size,
+  backgroundColor,
   color,
-  textColor,
 }: IconInnerProps) => {
-  const [valid, setValid] = React.useState<boolean>(true);
-
-  const rimble = <RimbleIcon name={name as RimbleIconProps["name"]} size={size} color={textColor} />;
-
-  if (rimble) {
-    return rimble;
+  
+  if (isWatcherIcon(watcherTransform(name))) {
+    const watcherProps: PropsOf<typeof StyledWatcherIcon> = {
+      name: watcherTransform(name),
+      width: size,
+      height: size,
+      ...( color && { color: color } ),
+      ...( backgroundColor && { color: backgroundColor } ) // This trick ensures that color isn't passed in if not set (preventing undefined overwrite).
+    };
+    return (
+      <StyledWatcherIcon
+        {...watcherProps}
+      />);
   }
 
-  if (valid) {
+  if (isEtherscanIcon(name)) {
     return (
       <Image
         size={size}
-        src={`https://etherscan.io/token/images/${name.toLowerCase()}_32.png`}
-        onError={() => setValid(false)}
+        src={`https://etherscan.io/token/images/${etherscanTransform(name)}_32.png`}
       />
     );
   }
-
-  if (name === "Algo") {
-    return (
-      <Circle
-        size={size}
-        fontSize={size}
-        backgroundColor={color}
-      >
-        <Image
-          src={`https://algoexplorer.io/images/${name.toLowerCase()}.png`}
-          size={size}
-        />
-      </Circle>
-    );
-  }
-
+  
+  const displayName = name.substring(0, 4);
   return (
     <Circle
-      size={size}
+      size="1em"
       fontSize={size}
-      backgroundColor={color}
+      backgroundColor={backgroundColor || "brand"}
     >
       <Text
-        fontSize=".7em"
+        fontSize={_.get(LEN2FONT, displayName.length) || "1em"}
         fontWeight="bold"
-        color={textColor}
-      >{name[0]}</Text>
+        color={color || "white"}
+      >{displayName}</Text>
     </Circle>
   );
-  // return <RimbleIcon name="Generic" size={size} color={color} />;
-
 };
 
 export const Icon: React.FC<IconProps> = ({
   name,
-  size = "2em",
-  innerSize = "100%",
+  size = "1em",
   color,
   backgroundColor,
   ...rest
 }: IconProps) => {
   return (
-    <Box size={size} {...rest}>
-      <IconInner name={name as CryptoIcons} size={innerSize} color={backgroundColor} textColor={color}/>
+    <Box
+      {...rest}
+    >
+      <IconInner
+        name={name}
+        size={size}
+        backgroundColor={backgroundColor}
+        color={color}
+      />
     </Box> 
   );
 };

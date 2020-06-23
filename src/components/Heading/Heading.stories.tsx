cd7e5f36d081343de21bc99376e3a9ddcd8c43cb
DJRHails <daniel@hails.info>
2020-06-23 10:30:05 +0100

    Update Heading text to be a pangram

--- src/components/Heading/Heading.stories.tsx
+++ src/components/Heading/Heading.stories.tsx
@@ -11,10 +11,10 @@ export const h1: StoryMeta = () => <Heading.h1>Heading h1</Heading.h1>;
 
 export const headingsList: StoryMeta = () => (
   <>
-    <Heading.h1>Heading h1</Heading.h1>
-    <Heading.h2>Heading h2</Heading.h2>
-    <Heading.h3>Heading h3</Heading.h3>
-    <Heading.h4>Heading h4</Heading.h4>
-    <Heading.h5>Heading h5</Heading.h5>
+    <Heading.h1>H1 - Quick zephyrs blow, vexing daft Jim.</Heading.h1>
+    <Heading.h2>H2 - Quick zephyrs blow, vexing daft Jim.</Heading.h2>
+    <Heading.h3>H3 - Quick zephyrs blow, vexing daft Jim.</Heading.h3>
+    <Heading.h4>H4 - Quick zephyrs blow, vexing daft Jim.</Heading.h4>
+    <Heading.h5>H5 - Quick zephyrs blow, vexing daft Jim.</Heading.h5>
   </>
 );

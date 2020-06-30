60d75ea9e21bdd576b3d03de11955ec27f3d87e2
DJRHails <daniel@hails.info>
2020-06-30 19:33:29 +0100

    Refactor Text into proper styled component

--- src/components/Text/Text.tsx
+++ src/components/Text/Text.tsx
@@ -1,52 +1,45 @@
-import React from "react";
 import { BoxProps, Box } from "../Box"; // TODO: This cannot be "/components" for some unknown reason
 import styled from "styled-components";
-import { TypographyProps, typography, ColorProps, color, variant, SpaceProps, space } from "styled-system";
+import { variant, BackgroundProps, background, typography, TypographyProps } from "styled-system";
 
-export type TextVariants = "heading" | "body" | "button";
+export type TextVariants = "heading" | "body" | "button" | "gradient";
 
-export interface TextProps extends BoxProps, TypographyProps, ColorProps, SpaceProps {
+export interface TextProps extends BoxProps, TypographyProps, BackgroundProps {
   uppercase?: true;
+  truncate?: true;
   variant?: TextVariants;
-} 
+}
 
-const StyledText = styled(Box)<TextProps>(
-  ({ uppercase }) => uppercase && "text-transform: uppercase;",
-  variant({
-    variants: {
-      heading: {
-        fontFamily: "body",
-        fontWeight: "bold",
-        lineHeight: "none",
-        m: 0,
-        mb: 1,
-      },
-      body: {
-        fontFamily: "body",
-        fontSize: 1,
-      },
-      button: {
-        position: "relative",
-        fontSize: 1,
-        fontWeight: "bold",
-        zIndex: 1,
-        userSelect: "none",
-        pointerEvents: "none",
-      }
+const textVariants = variant({
+  variants: {
+    body: {
+      fontFamily: "body",
+      fontSize: 1,
     },
-  }),
-  typography,
-  color,
-  space,
-);
+    button: {
+      position: "relative",
+      fontSize: 1,
+      fontWeight: "bold",
+      zIndex: 1,
+      userSelect: "none",
+      pointerEvents: "none",
+    },
+    gradient: {
+      "-webkit-background-clip": "text",
+      "-webkit-text-fill-color": "transparent",
+    }
+  },
+});
 
-export const Text: React.FC<TextProps> = ({
-  ...rest
-}: TextProps) => {
-  return (
-    <StyledText
-      {...rest}
-    />
-  );
-};
+export const Text = styled(Box)<TextProps>`
+  ${({ uppercase }) => uppercase && "text-transform: uppercase;"}
+  ${({ truncate }) => truncate && `
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+  `}
+  ${textVariants}
+  ${background}
+  ${typography}
+`;
 Text.displayName="Text";

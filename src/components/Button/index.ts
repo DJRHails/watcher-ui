46168b066be32d16caf1968d4aa4bd80bb7f86f4
DJRHails <daniel@hails.info>
2020-11-22 23:44:17 +0000

    Export gradient button

--- src/components/Button/index.ts
+++ src/components/Button/index.ts
@@ -1,5 +1,6 @@
 export { Button } from './Button';
 export { SolidButton } from './SolidButton';
 export { OutlineButton } from './OutlineButton';
+export { GradientButton } from './GradientButton';
 export { CloseButton } from './CloseButton';
 export type { ButtonProps } from './BaseButton';
